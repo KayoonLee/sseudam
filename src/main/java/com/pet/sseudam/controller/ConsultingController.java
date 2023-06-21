4942bb6c630cb5d7b7240312424fa78e7bf90580
win_nie <kayoon0111@gmail.com>
2023-06-21 18:01:28 +0900

    0621 이미지 삭제

--- src/main/java/com/pet/sseudam/controller/ConsultingController.java
+++ src/main/java/com/pet/sseudam/controller/ConsultingController.java
@@ -73,7 +73,6 @@ public class ConsultingController {
         List<PetBean> pet_list = con.find_pet(member.getM_id());
         model.addAttribute("pet_list", pet_list);
         System.out.println(pet_list);
-
         List<Date> reservation_time;
         reservation_time = con.find_reservation_time(member.getM_id(), con_id);
         for (Date c : reservation_time) {
